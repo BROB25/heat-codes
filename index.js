f8b675b16ee7c9a91d54a100374a8ebe5b9059fc
Brandon Roberts <bbrenterprise25@gmail.com>
2023-04-26 14:15:45 -0500

    Your commit message

---
+++ index.js
@@ -0,0 +1,48 @@
+// swiper js
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+});
+
+// Portfolio button
+const scrollToPortfolio = () => {
+  const portfolioSection = document.getElementById('portfolio');
+  portfolioSection.scrollIntoView({behavior: "smooth"});
+}
+
+// Contact button
+const scrollToContact = () => {
+  const contactSection = document.getElementById('contact');
+  contactSection.scrollIntoView({behavior: "smooth"});
+}
+
+// Mobile Menu
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+const menuLinks = document.querySelectorAll('.nav-link');
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    const menu = document.querySelector('.nav-menu');
+    const hamburger = document.querySelector('.hamburger');
+    menu.classList.remove('active');
+    hamburger.classList.remove('active');
+  });
+});
