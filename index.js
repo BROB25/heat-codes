7e61bb969d72ee72d9d5fb30c4578cfb54b07160
Brandon Roberts <bbrenterprise25@gmail.com>
2023-04-27 14:50:11 -0500

    dark mode and other changes

--- index.js
+++ index.js
@@ -46,3 +46,11 @@ menuLinks.forEach((link) => {
     hamburger.classList.remove('active');
   });
 });
+
+// ======== DarkMode ======== \\
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+}
